f875cc78cccee71bcb8e5bfbb9a2514891795735
XEarthlydust <1767240342@qq.com>
2023-06-25 17:18:08 +0800

    feat(`main`): 更新Main调用

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from page_schedule.tool.terminal_input import terminal_input as Terminal_input
-from page_schedule.tool.random_input import random_input as Random_input
 from page_schedule.core.controller import controller
 from page_schedule.core.memory_frame import memory_frame
+from page_schedule.tool.random_input import random_input as Random_input
+from page_schedule.tool.terminal_input import terminal_input as Terminal_input
 
 
 def by_user():
@@ -24,11 +24,30 @@ def by_random():
 
 if __name__ == '__main__':
 
-    info_input = by_user()
+    info_input = None
+
+    while True:
+        try:
+            print("1. 用户输入 | 2. 随机生成")
+            choice = int(input("请输入数据来源模式: "))
+            if choice == 1:
+                info_input = by_user()
+            elif choice == 2:
+                info_input = by_random()
+            else:
+                raise ValueError
+        except ValueError:
+            print("类型错误 请重新输入")
+            continue
+        print("无输入回车后确认执行 或 输入任意内容回车后重新输入")
+        enter = input()
+        if enter == "":
+            break
+        else:
+            continue
     ctrl = info_input[0]
     alg = info_input[1]
-    print("按任意键确认执行")
-    input()
+
     while True:
         try:
             print(ctrl(alg))
@@ -37,6 +56,3 @@ if __name__ == '__main__':
             break
     print()
     ctrl.print_all()
-
-
-
